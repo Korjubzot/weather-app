acfd5f41bd9482ef3e637944e70568342fb01bfc
Korjubzot <korjubzot@gmail.com>
2023-08-04 11:32:07 +0100

    Added forecast data + further cleanup of files

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable prettier/prettier */
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import "./styles/index.css";
+import App from "./components/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
