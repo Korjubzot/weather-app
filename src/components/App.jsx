fca44cccb4b028a4877f716e15c8cfc2936e582f
Korjubzot <korjubzot@gmail.com>
2023-08-06 15:14:42 +0100

    Added location data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import "../styles/App.css";
 import React from "react";
+import LocationDetails from "./LocationDetails";
 
-function App() {
+function App(props) {
+  const { location } = props;
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <LocationDetails city={location.city} country={location.country} />
     </div>
   );
 }
