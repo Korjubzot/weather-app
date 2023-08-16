913a4c2fb64f0f967ef2d57c97a565fe22b924ad
Korjubzot <korjubzot@gmail.com>
2023-08-16 20:43:14 +0100

    Fixed issue relating to weatherCode displaying incorrect icon

--- src/components/ForecastSummary.jsx
+++ src/components/ForecastSummary.jsx
@@ -3,9 +3,26 @@ import iconData from "../data/iconData.json";
 
 function ForecastSummary(props) {
   const { date, description, icon, temperature, onSelect } = props;
-  const weatherCode = `${icon.slice(0, 1)}00`;
+  const weatherCode = `${icon.toString().slice(0, 1)}00`;
   const formattedDate = new Date(date).toDateString();
 
+  if (icon > 800) {
+    return (
+      <div className="forecast-summary" data-testid="forecast-summary">
+        <div className="forecast-summary__date">{formattedDate}</div>
+        <div className="forecast-summary__icon" data-testid="forecast-icon">
+          <img src={iconData["8xx"]} alt="" />
+        </div>
+        <div className="forecast-summary__temperature">
+          {temperature.max}&deg;C
+        </div>
+        <div className="forecast-summary__description">{description}</div>
+        <button type="button" onClick={() => onSelect(date)}>
+          More details
+        </button>
+      </div>
+    );
+  }
   return (
     <div className="forecast-summary" data-testid="forecast-summary">
       <div className="forecast-summary__date">{formattedDate}</div>
--- src/components/LocationDetails.jsx
+++ src/components/LocationDetails.jsx
@@ -4,9 +4,20 @@ function LocationDetails(props) {
   LocationDetails.defaultProps = {
     errorMessage: "",
   };
+
+  function refreshPage() {
+    window.location.reload(false);
+  }
   const { city, country, errorMessage } = props;
   return errorMessage ? (
-    <h1>{errorMessage}</h1>
+    <>
+      <h1>{errorMessage}</h1>
+      <div>
+        <button type="button" onClick={refreshPage}>
+          Click to reload
+        </button>
+      </div>
+    </>
   ) : (
     <h1>{`${city}, ${country}`} </h1>
   );
